681d8824b77a3169649c8fa6f4a7e0aca4b7fe1c
Guilherme <guilhermeyoshizawa10@gmail.com>
2026-04-04 19:43:56 -0300

    feat: finaliza crud e ajusta variáveis de ambiente

---
+++ backend/src/main/java/com/reusehub/anuncio/repository/EnderecoRepository.java
@@ -0,0 +1,12 @@
+package com.reusehub.anuncio.repository;
+
+import com.reusehub.anuncio.model.Endereco;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface EnderecoRepository extends JpaRepository<Endereco, UUID> {
+
+}
