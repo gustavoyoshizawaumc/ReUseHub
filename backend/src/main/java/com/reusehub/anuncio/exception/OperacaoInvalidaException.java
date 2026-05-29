c3e5c0b6c8310db05d7141d4f91f0d5c28d820d4
GuilhermeYoshizawaUMC <11222102064@alunos.umc.br>
2026-05-29 19:39:34 -0300

    refactor(viacep): aplica clean code

--- backend/src/main/java/com/reusehub/anuncio/exception/OperacaoInvalidaException.java
+++ backend/src/main/java/com/reusehub/anuncio/exception/OperacaoInvalidaException.java
@@ -1,7 +1,12 @@
 package com.reusehub.anuncio.exception;
 
 public class OperacaoInvalidaException extends RuntimeException {
+
     public OperacaoInvalidaException(String mensagem) {
         super(mensagem);
+    }
+
+    public OperacaoInvalidaException(String mensagem, Throwable causa) {
+        super(mensagem, causa);
     }
 }
